92068e28dc97375bcc5d0a2bbc854c70653c887d
YYAISYAH <yyaisyaholivia@gmail.com>
2024-11-11 01:17:37 +0700

    Add files via upload

---
+++ Jquery project.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirect</title>
+    <script src="jquery.js"></script>
+    <style>
+        h1 {
+            font-family: 'arial';
+            font-size: 2.5em;
+            color: #4a90e2;
+            text-align: center;
+            margin-top: 20%;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+        }
+    </style>
+    <script>
+        $(document).ready(function() {
+            setTimeout(function() {
+                window.location.href = "https://ypaus.online";
+            }, 10000);
+        });
+    </script>
+</head>
+<body>
+    <h1>Dipersembahkan seoaisyah :3</h1>
+</body>
+</html>
